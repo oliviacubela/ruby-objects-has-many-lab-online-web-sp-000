48f9a7a42a48c0b11741c7976d31b6d7edac2198
Olivia Cubela <oliviacubela@gmail.com>
2020-07-13 14:27:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,6 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count += 1
   end
 
   def self.song_count
@@ -15,11 +14,13 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
+    @@song_count += 1
   end
 
   def songs
